6b0956092d81364e3d6fffaa9ce38efd84b59222
Ronaldinho <ronaldinho_x86@qq.com>
2022-09-30 15:23:55 +0800

    update twitch demo

--- python/twitch.py
+++ python/twitch.py
@@ -1,3 +1,4 @@
+import base64
 import json
 import os
 import time
@@ -12,11 +13,14 @@ UnicornSdk.auth(os.environ.get("ACCESS_TOKEN"))
 UnicornSdk.config_sdk(api_url="https://dev.unicorn-bot.com")
 # UnicornSdk.config_sdk(api_url="http://localhost:9000")
 
+# session = Session()
 session = TlsSession()
+session.config_tls(http2=False)
 session.config_for_kasada({
     'gql.twitch.tv': {
         'POST': [
             '/gql',
+            "/integrity",
         ]
     },
     "passport.twitch.tv": {
@@ -60,6 +64,31 @@ time.sleep(10)
 resp = session.get(fp_url, proxies=proxies)
 logger.debug(resp.status_code)
 logger.debug(resp.text)
+
+resp = session.post(
+    "https://gql.twitch.tv/integrity",
+    headers={
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Referer": "https://www.twitch.tv/",
+        "Client-Id": "kimne78kx3ncx6brgo4mv6wki5h1ko",
+        "X-Device-Id": "SFXAMVyKvxdRcutbG4JZUseUjWg4jzhn",
+        "Client-Session-Id": "618d0eff052e1351",
+        "Client-Version": "40aeca85-399f-45a8-9f12-30caf2c0f495",
+        "x-kpsdk-ct": None,
+        "x-kpsdk-cd": None,
+        "Origin": "https://www.twitch.tv",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-site",
+        "Content-Length": "0",
+    },
+    proxies=proxies
+)
+logger.debug(resp.status_code)
+logger.debug(json.dumps(resp.json(), indent=4))
 
 
 
